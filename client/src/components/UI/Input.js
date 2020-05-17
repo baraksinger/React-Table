9036644dfa514ca3f8d8e9ffbcaa290fab1dfac4
Barak Singer <32904002+baraksinger@users.noreply.github.com>
2020-05-17 19:02:14 +0300

    Add files via upload

--- client/src/components/UI/Input.js
+++ client/src/components/UI/Input.js
@@ -1,1 +1,30 @@
+import {TextField} from "@material-ui/core";
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
 
+const useStyles = makeStyles(() => ({
+    textField: {
+        margin: "22px 0"
+    }
+}))
+
+const Input = ({changed, value}) => {
+
+    const classes = useStyles();
+
+    return (
+        <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Name"
+            type="text"
+            fullWidth
+            value={value}
+            className={classes.textField}
+            onChange={changed}
+        />
+    );
+}
+
+export default Input;
